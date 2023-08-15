51b70a4fdb81eb8babfdb0b122046c5c5673f544
Drakmyth <shaun.hamman@gmail.com>
2023-08-15 09:17:14 -0700

    Add htmx functionality

--- server.go
+++ server.go
@@ -10,7 +10,11 @@ func main() {
 	fmt.Println("Hello World!")
 
 	h1 := http.FileServer(http.Dir("public"))
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<p>I've Been Clicked!</p>"))
+	}
 
+	http.HandleFunc("/clicked", h2)
 	http.Handle("/", h1)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
