06570fd74c20a3dbaf6364817405232ec723550a
Drakmyth <shaun.hamman@gmail.com>
2023-08-17 06:56:52 -0700

    Generate Id on each item, add edit & delete buttons

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Drakmyth/todo-htmx-go
 
 go 1.21.0
+
+require github.com/google/uuid v1.3.0
--- server.go
+++ server.go
@@ -5,14 +5,25 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/google/uuid"
 )
+
+type Item struct {
+	Id          uuid.UUID
+	Description string
+}
 
 func main() {
 	fmt.Println("Hello World!")
 
 	addHandler := func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
-		item := r.Form.Get("item")
+		description := r.Form.Get("description")
+		item := Item{
+			Id:          uuid.New(),
+			Description: description,
+		}
 		tmpl, _ := template.ParseFiles("./templates/item.tmpl.html")
 		tmpl.Execute(w, item)
 	}
