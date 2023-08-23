415103e67f834e0c6e6383ae1e5aee814f7bff10
Drakmyth <shaun.hamman@gmail.com>
2023-08-23 13:27:02 -0700

    Load existing items on page refresh

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Drakmyth/todo-htmx-go
 
 go 1.21.0
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
+)
--- tasks.go
+++ tasks.go
@@ -6,11 +6,16 @@ import (
 	"text/template"
 
 	"github.com/google/uuid"
+	"golang.org/x/exp/maps"
 )
 
 type Task struct {
 	Id          uuid.UUID
 	Description string
+}
+
+type TaskList struct {
+	Tasks []Task
 }
 
 var tasks = make(map[uuid.UUID]Task)
@@ -23,7 +28,6 @@ func NewTaskMux() *http.ServeMux {
 }
 
 func createTask(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("Hello!")
 	r.ParseForm()
 
 	id := uuid.New()
@@ -34,14 +38,29 @@ func createTask(w http.ResponseWriter, r *http.Request) {
 	}
 	tasks[id] = task
 
-	tmpl, _ := template.ParseFiles("./templates/item.tmpl.html")
-	tmpl.Execute(w, task)
+	fmt.Printf("Created %s\n", task)
+
+	tmpl, _ := template.ParseFiles("./templates/task.tmpl.html")
+	tmpl.ExecuteTemplate(w, "task", task)
+}
+
+func getTaskList(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Retrieving %d tasks...\n", len(tasks))
+
+	taskList := TaskList{
+		Tasks: maps.Values(tasks),
+	}
+
+	tmpl, _ := template.ParseFiles("./templates/task.tmpl.html")
+	tmpl.ExecuteTemplate(w, "task-list", taskList)
 }
 
 func taskRootHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
 		createTask(w, r)
+	case http.MethodGet:
+		getTaskList(w, r)
 	default:
 		methodNotAllowedHandler(w, r)
 	}
